bf71b25f915ee8665ae44409eb4d2baf0530d023
Wang Yu <notchunyu@gmail.com>
2026-09-24 20:32:38 +0800

    Update Outcome.jsx

--- src/components/Outcome.jsx
+++ src/components/Outcome.jsx
@@ -6,7 +6,7 @@ export const Outcome = () => {
     <div style={{ maxWidth: '100%', textAlign: 'center' }}>
       <h2>Yay! I knew you would say yes!</h2>
       <div>
-        <Gif url='https://media.giphy.com/media/3o6wNIK2unphUcCcqQ/giphy.gif' />
+        <Gif url='https://media1.giphy.com/media/v1.Y2lkPTc5MGI3NjExZDAwbzE1bWxkNmtrM20xYWdld253MDBvN2lxODUxOWVyMndqbzQwaCZlcD12MV9pbnRlcm5hbF9naWZfYnlfaWQmY3Q9Zw/KztT2c4u8mYYUiMKdJ/giphy.gif' />
       </div>
     </div>
   );
